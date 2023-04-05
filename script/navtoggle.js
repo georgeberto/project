03ab1d166589b0b8d1698b9099595758465fae5e
georgeberto <yatogami2029@gmail.com>
2023-04-05 15:10:38 +0700

    kelar(+responsive)

---
+++ script/navtoggle.js
@@ -0,0 +1,8 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navLinks = document.getElementsByClassName('nav-links')[0]
+const links = document.getElementsByTagName('ul')
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+    links.classList.toggle('active')
+})
